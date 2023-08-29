6b69ff6f56dd4ac4fcc849eaef6d19a0ea36c3e7
Preocts <preocts@preocts.com>
2023-08-28 21:23:08 -0400

    Overcomplicate noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,44 @@
 from __future__ import annotations
 
+import os
+import sys
+
 import nox
 
+# Control factors for finding pieces of the module
 MODULE_NAME = "module_name"
-COVERAGE_FAIL_UNDER = "--fail-under=50"
+TESTS_PATH = "tests"
+COVERAGE_FAIL_UNDER = 50
+REQUIREMENT_IN_FILES = [
+    os.path.join("requirements", "requirements.in"),
+    os.path.join("requirements", "requirements-dev.in"),
+    os.path.join("requirements", "requirements-test.in"),
+]
+
+# What are we allowed to clean (delete)?
+CLEAN_DIRS = [
+    "__pycache__",
+    ".mypy_cache",
+    ".pytest_cache",
+    ".coverage",
+    ".nox",
+    "dist",
+    "build",
+]
+CLEAN_FILES = [
+    "*.pyc",
+    "*.pyo",
+    "coverage.json",
+    ".coverage.*",
+]
+
+
+# Define the default sessions run when `nox` is called on the CLI
+nox.options.sessions = [
+    "tests_with_coverage",
+    "coverage_combine_and_report",
+    "mypy_check",
+]
 
 
 @nox.session(
@@ -14,7 +49,7 @@ def tests_with_coverage(session: nox.Session) -> None:
     print_standard_logs(session)
 
     session.install(".[test]")
-    session.run("coverage", "run", "-p", "-m", "pytest", "tests/")
+    session.run("coverage", "run", "-p", "-m", "pytest", TESTS_PATH)
 
 
 @nox.session()
@@ -22,9 +57,11 @@ def coverage_combine_and_report(session: nox.Session) -> None:
     """Combine all coverage partial files and generate JSON report."""
     print_standard_logs(session)
 
+    fail_under = f"--fail-under={COVERAGE_FAIL_UNDER}"
+
     session.install(".[test]")
     session.run("python", "-m", "coverage", "combine")
-    session.run("python", "-m", "coverage", "report", "-m", COVERAGE_FAIL_UNDER)
+    session.run("python", "-m", "coverage", "report", "-m", fail_under)
     session.run("python", "-m", "coverage", "json")
 
 
@@ -36,6 +73,69 @@ def mypy_check(session: nox.Session) -> None:
     session.install(".")
     session.install("mypy")
     session.run("mypy", "-p", MODULE_NAME, "--no-incremental")
+
+
+@nox.session(python=False)
+def coverage(session: nox.Session) -> None:
+    """Generate a coverage report. Does not use a venv."""
+    session.run("coverage", "erase")
+    session.run("coverage", "run", "-m", "pytest", TESTS_PATH)
+    session.run("coverage", "report", "-m")
+
+
+@nox.session(python=False)
+def docker(session: nox.Session) -> None:
+    """Run tests in a docker container. Requires docker damon running."""
+    session.run("docker", "build", "-t", "pydocker-test", ".")
+    session.run("docker", "run", "-it", "--rm", "pydocker-test")
+
+
+@nox.session()
+def build(session: nox.Session) -> None:
+    """Build distrobution files."""
+    print_standard_logs(session)
+
+    session.install("build")
+    session.run("python", "-m", "build")
+
+
+@nox.session()
+def update(session: nox.Session) -> None:
+    """Process requirement*.in files, updating only additions/removals."""
+    print_standard_logs(session)
+
+    session.install("pip-tools")
+    for filename in REQUIREMENT_IN_FILES:
+        session.run("pip-compile", "--no-emit-index-url", filename)
+
+
+@nox.session()
+def upgrade(session: nox.Session) -> None:
+    """Process requirement*.in files and upgrade all libraries as possible."""
+    print_standard_logs(session)
+
+    session.install("pip-tools")
+    for filename in REQUIREMENT_IN_FILES:
+        session.run("pip-compile", "--no-emit-index-url", "--upgrade", filename)
+
+
+@nox.session(python=False)
+def clean(session: nox.Session) -> None:
+    """Clean cache, .pyc, .pyo, and artifact files from project."""
+    if sys.platform.startswith("win"):
+        print("Windows unsupported at this time.")
+        return None
+
+    elif sys.platform in ["linux", "darwin"]:
+        for dirpath in CLEAN_DIRS:
+            session.run("find", ".", "-name", dirpath, "-exec", "rm", "-rf", "{}", "+")
+
+        for file in CLEAN_FILES:
+            session.run("find", ".", "-name", file, "-exec", "rm", "-f", "{}", "+")
+
+    else:
+        print("Unknown OS for this session.")
+        return None
 
 
 def print_standard_logs(session: nox.Session) -> None:
